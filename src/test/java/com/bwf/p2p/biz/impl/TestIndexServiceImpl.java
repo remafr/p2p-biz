5b5095951f0f58d4069143bd91ad4c913d2f961d
remafr <799743258@qq.com>
2018-05-01 23:36:20 +0800

    all

---
+++ src/test/java/com/bwf/p2p/biz/impl/TestIndexServiceImpl.java
@@ -0,0 +1,11 @@
+package com.bwf.p2p.biz.impl;
+
+import org.junit.Test;
+
+public class TestIndexServiceImpl {
+	@Test
+	public void test() {
+		//ApplicationContext ac=ApplicationContextUtil.getApplicationContext();
+		//assertEquals(null, ac);
+	}
+}
---
+++ src/test/java/com/bwf/p2p/util/TestApplicationContextUtil.java
@@ -0,0 +1,17 @@
+package com.bwf.p2p.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestApplicationContextUtil {
+	@Test
+	public void test() {
+		//ApplicationContext ac=new ClassPathXmlApplicationContext("p2p-web.xml","p2p-core.xml");
+		
+		//assertEquals(null, ac);
+	}
+	
+}
